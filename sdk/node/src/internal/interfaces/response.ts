2e8da131ab50056d6be895a54c6236d5b9bbede5
Isaac.Tang <Isaac.Tang@kupotech.com>
2025-01-09 15:33:36 +0800

    feat(nodejs): update interface

--- sdk/node/src/internal/interfaces/response.ts
+++ sdk/node/src/internal/interfaces/response.ts
@@ -1,12 +1,11 @@
 import { Serializable } from '@internal/interfaces/serializable';
 
 /**
- * Abstract class Response
  * Represents a response handler with a method to set common response data.
  */
-export abstract class Response<T, R> extends Serializable<T> {
+export interface Response<T, R> extends Serializable<T> {
     /**
      * Set common response data.
      */
-    abstract setCommonResponse(response: R): void;
+    setCommonResponse(response: R): void;
 }
--- sdk/node/src/internal/interfaces/serializable.ts
+++ sdk/node/src/internal/interfaces/serializable.ts
@@ -1,19 +1,19 @@
 /**
- * An abstract base class for serializable objects.
+ * The interface for serializable objects.
  */
-export abstract class Serializable<T> {
+export interface Serializable<T> {
     /**
      * Parses a JSON string and populates the object's properties.
      */
-    abstract fromJson(input: string): T;
+    fromJson(input: string): T;
 
     /**
      * Converts the current object into a JSON string.
      */
-    abstract toJson(): string;
+    toJson(): string;
 
     /**
      * Populates the object's properties.
      */
-    abstract fromObject(jsonObject: Object): T;
+    fromObject(jsonObject: Object): T;
 }
